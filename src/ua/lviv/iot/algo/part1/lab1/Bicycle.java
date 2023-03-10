071e053bc5e3011252bd458916fc427c0a26a859
annakozakkk <kozakanna011@gmail.com>
2023-03-10 13:31:39 +0200

    add some changes

--- src/ua/lviv/iot/algo/part1/lab1/Bicycle.java
+++ src/ua/lviv/iot/algo/part1/lab1/Bicycle.java
@@ -7,42 +7,51 @@ import lombok.*;
 @ToString
 public class Bicycle {
 
-    private static Bicycle instance = new Bicycle("moutain", "Trek", 40, 20);
+    private static Bicycle instance = new Bicycle("mountain", "Trek", 40.8, 20.8);
 
     public static Bicycle getInstance() {
         return instance;
     }
 
-    private String type="road";
-    private String brand="Giant";
-    private int maxSpeed=50;
-    private int currentSpeed=22;
+    private String type;
+    private String brand;
+    private double maxSpeed;
+    private double currentSpeed;
 
 
-    public int accelerate(int speed) {
+    public double accelerate(int speed) {
         if (speed < maxSpeed) {
             currentSpeed = currentSpeed + speed;
+
         }
         return currentSpeed;
     }
 
-    public int brake() {
+    public double brake() {
 
         return currentSpeed=0;
     }
-    public double increasePrice(double price)
-    {
-        return price+50;
-    }
+      double slowDown(double speed){
+              if (speed<currentSpeed) {
+                  currentSpeed = currentSpeed-speed;
+              }
+              return currentSpeed;
+          }
+
+
     public static void main(String[] arg) {
         Bicycle[] bicycles= new Bicycle[4];
         bicycles[0]=Bicycle.getInstance();
         bicycles[1]=Bicycle.getInstance();
-        bicycles[2]=new Bicycle("moutain","Colnago",45,34);
+        bicycles[2]=new Bicycle("mountain","Colnago",45,34);
         bicycles[3]=new Bicycle();
 
         for(Bicycle i:bicycles){
             System.out.println(i.toString());
         }
+        System.out.println("The speed of bicycle 'Trek' after accelerating  is " +bicycles[1].accelerate(8));
+        System.out.println("The bicycle 'Trek' was braked to a complete stop,speed is: " + bicycles[1].brake());
+        System.out.println("The speed of the bicycle 'Colnago' after it has been slowed down is: "+bicycles[2].slowDown(23));
+
     }
 }
