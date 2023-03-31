7bd97356e6f9a26d0c4e305982d8782f83e7f6d5
annakozakkk <kozakanna011@gmail.com>
2023-03-31 15:26:07 +0300

    add second laboratorna

--- src/ua/lviv/iot/algo/part1/lab1/Bicycle.java
+++ src/ua/lviv/iot/algo/part1/lab1/Bicycle.java
@@ -1,58 +1,17 @@
 package ua.lviv.iot.algo.part1.lab1;
-
 import lombok.*;
-
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
-@ToString
-public class Bicycle {
-    private static Bicycle instance = new Bicycle("mountain", "Trek", 40.8, 20.8);
-    public static Bicycle getInstance() {
-        return instance;
+@ToString (callSuper = true)
+public class Bicycle extends AbstractBicycle {
+    public Bicycle(String brand, double maxSpeed, double currentSpeed) {
+        super(brand, maxSpeed, currentSpeed);
     }
 
-    private String type;
-    private String brand;
-    private double maxSpeed;
-    private double currentSpeed;
-
-    public double accelerate(int speed) {
-        if (speed < maxSpeed) {
-            currentSpeed += speed;
-            return currentSpeed;
-        }
-            return maxSpeed;
-    }
-
-    public double brake() {
-        return currentSpeed = 0;
-    }
-
-    double slowDown(double speed) {
-        if (speed > currentSpeed) {
-            return brake();
-        }
-        currentSpeed -= speed;
-        return currentSpeed;
-    }
-
-
-    public static void main(String[] arg) {
-        var bicycles = new Bicycle[4];
-        bicycles[0] = Bicycle.getInstance();
-        bicycles[1] = Bicycle.getInstance();
-        bicycles[2] = new Bicycle("mountain", "Colnago", 45, 34);
-        bicycles[3] = new Bicycle();
-
-        for (var bicycle : bicycles) {
-            System.out.println(bicycle.toString());
-        }
-
-        System.out.println("The speed of bicycle 'Trek' after accelerating  is " + bicycles[1].accelerate(60));
-        System.out.println("The bicycle 'Trek' was braked to a complete stop,speed is: " + bicycles[1].brake());
-        System.out.println("The speed of the bicycle 'Colnago' after it has been slowed down is: " + bicycles[2].slowDown(23));
-
+    @Override
+    public double getMaxDistance() {
+        return Integer.MAX_VALUE;
     }
 }
+
---
+++ src/ua/lviv/iot/algo/part1/lab1/ElectricBicycle.java
@@ -0,0 +1,20 @@
+package ua.lviv.iot.algo.part1.lab1;
+
+import lombok.ToString;
+
+@ToString(callSuper = true)
+public class ElectricBicycle extends AbstractBicycle {
+    public ElectricBicycle(String brand, double maxSpeed, double currentSpeed, double capacityOfBattery,double energyConsumptionPer100meters) {
+        super(brand, maxSpeed, currentSpeed);
+        this.capacityOfBattery = capacityOfBattery;
+        this.energyConsumptionPer100meters=energyConsumptionPer100meters;
+    }
+
+    private double capacityOfBattery;
+
+    private  double energyConsumptionPer100meters;
+    @Override
+    public double getMaxDistance() {
+        return capacityOfBattery*energyConsumptionPer100meters;
+    }
+}
